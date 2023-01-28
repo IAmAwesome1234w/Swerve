a67468c22fded4ce480bb7978de5f6cdb3d916ee
IAmAwesome1234w <iamawesome1234w@gmail.com>
2023-01-28 16:49:47 -0600

    modified:   src/main/java/frc/robot/subsystems/SwerveModule.java 	modified:   src/main/java/frc/robot/Robot.java 	deleted:    src/main/java/frc/robot/commands/Autos.java 	deleted:    src/main/java/frc/robot/commands/ExampleCommand.java

--- src/main/java/frc/robot/subsystems/SwerveModule.java
+++ src/main/java/frc/robot/subsystems/SwerveModule.java
@@ -4,20 +4,84 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.sensors.CANCoder;
+import com.revrobotics.CANEncoder;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.wpilibj.CAN;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.SwerveConstants;
 
 public class SwerveModule extends SubsystemBase {
   /** Creates a new SwerveModule. */
-  private final SwerveModule leftfront_drive = new SwerveModule(
-    SwerveConstants.LEFT_FRONT_DRIVE_ID, 
-    SwerveConstants.LEFT_FRONT_TURN_ID,
-    SwerveConstants.LEFT_FRONT_CANCODER_ID,
-    SwerveConstants.
-  );
+  private CANSparkMax driveMotor;
+  private CANSparkMax turnMotor;
+
+  private RelativeEncoder driveEncoder;
+  private RelativeEncoder turnEncoder;
+
+  private PIDController turnPIDController;
+  private CANCoder absoluteEncoder;
+
+  private boolean absoluteEncoderReversed;
+  private double absoluteEncoderOffset;
 
   
-  public SwerveModule() {}
+  public SwerveModule(int driveMotorID, int turnMotorID,
+   boolean driveMotorReversed,boolean turnMotorReversed,
+    int absoluteEncoderID, double absoluteEncoderOffset,
+     boolean absoluteEncoderReversed)
+     {
+        this.absoluteEncoderOffset = absoluteEncoderOffset;
+        this.absoluteEncoderReversed = absoluteEncoderReversed;
+
+        driveMotor = new CANSparkMax(driveMotorID, MotorType.kBrushless);
+        turnMotor = new CANSparkMax(turnMotorID, MotorType.kBrushless);
+
+        absoluteEncoder = new CANCoder(absoluteEncoderID);
+
+        driveEncoder = driveMotor.getEncoder();
+        turnEncoder = turnMotor.getEncoder();
+
+        turnPIDController = new PIDController(SwerveConstants.KP_TURNING, 0, 0);
+        turnPIDController.enableContinuousInput(-Math.PI, Math.PI);
+        
+        resetEncoders();
+     }
+
+     public double getDriveMotorVelocity()
+     {
+      return driveEncoder.getVelocity() * SwerveConstants.TURN_MOTOR_VCONVERSION;
+     }
+    public double getTurnMotorPosition()
+    {
+      return turnEncoder.getPosition() * SwerveConstants.TURN_MOTOR_PCONVERSION;
+    }
+     public double getAbsoluteEncoderAngle()
+     {
+       double angle = absoluteEncoder.getAbsolutePosition();
+       angle = angle - absoluteEncoderOffset;
+       angle = angle*(Math.PI/180);
+       return angle*(absoluteEncoderReversed ? -1.0 : 1.0);
+     }
+
+     public void resetEncoders()
+     {
+      driveEncoder.setPosition(0);
+      turnEncoder.setPosition(getAbsoluteEncoderAngle()/SwerveConstants.TURN_MOTOR_PCONVERSION);
+     }
+
+     public SwerveModuleState getSwerveState()
+     {
+      return new SwerveModuleState(getDriveMotorVelocity(), new Rotation2d(getTurnMotorPosition()));
+     }
+    
+     
 
   @Override
   public void periodic() {
